1d8c5572c4635f77bece5ee817900a8b11e3000b
Galder Zamarreño <galder@zamarreno.com>
2017-01-24 16:35:56 +0100

    [HB] CH13 - Add prompt to command line app

--- haskellbook-hello/src/Main.hs
+++ haskellbook-hello/src/Main.hs
@@ -2,9 +2,13 @@ module Main where
 
 import DogsRule
 import Hello
+import System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdout NoBuffering
+  -- ^ so that putStr isn't buffer (deferred) and prints immediately
+  putStr "Please inpurt your name: "
   name <- getLine
   sayHello name
   dogs
