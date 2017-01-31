0e6545a7dc2c283de74b24cf6e56ca890d638150
Galder Zamarreño <galder@zamarreno.com>
2017-01-31 09:07:34 +0100

    [HB] CH14 - Add first spec test

--- haskellbook/testing-hb-ch14/Addition.hs
+++ haskellbook/testing-hb-ch14/Addition.hs
@@ -1,4 +1,13 @@
 module Addition where
 
+import Test.Hspec
+
 sayHello :: IO ()
 sayHello = putStrLn "hello!"
+
+
+main :: IO ()
+main = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
