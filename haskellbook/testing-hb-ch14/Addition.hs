8b44f51dabc1794a205fb0dc9ad903c617bcea7a
Galder Zamarreño <galder@zamarreno.com>
2017-02-01 16:12:21 +0100

    [HB] CH14 - Spec for multiplyBySum

--- haskellbook/testing-hb-ch14/Addition.hs
+++ haskellbook/testing-hb-ch14/Addition.hs
@@ -13,6 +13,16 @@ divideBy num denom = go num denom 0
           | otherwise = go (n - d) d (count + 1)
 
 
+multiplyBySum :: (Integral a) => a -> a -> a
+multiplyBySum 0 _ =
+  0
+multiplyBySum _ 0 =
+  0
+multiplyBySum n 1 =
+  n
+multiplyBySum n x =
+  n + (multiplyBySum n (x - 1))
+
 main :: IO ()
 main = hspec $ do
   describe "Addition" $ do
@@ -24,3 +34,9 @@ main = hspec $ do
       divideBy 15 3 `shouldBe` (5, 0)
     it "22 divided by 5 is 4 reminder 2" $ do
       divideBy 22 5 `shouldBe` (4, 2)
+    it "5 times 2 is 10" $ do
+      multiplyBySum 5 2 `shouldBe` 10
+    it "0 times 2 is 0" $ do
+      multiplyBySum 0 2 `shouldBe` 0
+    it "2 times 0 is 0" $ do
+      multiplyBySum 2 0 `shouldBe` 0
