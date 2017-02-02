bd8b0dc35a6217c39c75605fef80282ae3e09065
Galder Zamarreño <galder@zamarreno.com>
2017-02-02 17:18:09 +0100

    [HB] CH14 - Gen random values from limited set

--- haskellbook/testing-hb-ch14/Generators.hs
+++ haskellbook/testing-hb-ch14/Generators.hs
@@ -3,6 +3,13 @@ module Generators where
 
 import Test.QuickCheck
 
--- Execute from repl with: sample trivialInt
+-- Execute from repl with either of this:
+-- sample trivialInt
+-- sample' trivialInt
 trivialInt :: Gen Int
 trivialInt = return 1
+
+
+oneThroughThree :: Gen Int
+oneThroughThree =
+  elements [1, 2, 3]
