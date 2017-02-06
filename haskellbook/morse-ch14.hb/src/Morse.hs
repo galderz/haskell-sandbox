94504ab91290e3b81c51580b3f6386148126bb8b
Galder Zamarreño <galder@zamarreno.com>
2017-02-06 17:29:55 +0100

    [HB] CH14 - Morse char to English char functions

--- haskellbook/morse-ch14.hb/src/Morse.hs
+++ haskellbook/morse-ch14.hb/src/Morse.hs
@@ -7,6 +7,18 @@ import qualified Data.Map as M
 
 type Morse = String
 
+morseToLetter :: M.Map Morse Char
+morseToLetter = M.foldWithKey (flip M.insert) M.empty letterToMorse
+
+charToMorse :: Char -> Maybe Morse
+charToMorse c = M.lookup c letterToMorse
+
+stringToMorse :: String -> Maybe [Morse]
+stringToMorse s = sequence $ fmap charToMorse s
+
+morseToChar :: Morse -> Maybe Char
+morseToChar m = M.lookup m morseToLetter
+
 letterToMorse :: (M.Map Char Morse)
 letterToMorse =
   M.fromList
