540f7a1a60965ff0d7b19b1774dd9a0ebf9f8743
Galder Zamarreño <galder@zamarreno.com>
2017-02-08 16:08:04 +0100

    [HB] CH 14 - Arbitrary products

--- haskellbook/testing-ch14.hb/Arbitraries.hs
+++ haskellbook/testing-ch14.hb/Arbitraries.hs
@@ -38,7 +38,32 @@ identityGenInt =
     identityGen
 
 
+-- Arbitrary Products
+
+
+data Pair a b =
+    Pair a b
+    deriving (Eq, Show)
+
+
+pairGen :: (Arbitrary a, Arbitrary b) => Gen (Pair a b)
+pairGen =
+    do  a <- arbitrary
+        b <- arbitrary
+        return (Pair a b)
+
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+    arbitrary = pairGen
+
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString =
+    pairGen
+
+
 main :: IO ()
 main =
-    sample identityGenInt
+    sample pairGenIntString
+    -- sample identityGenInt
     -- sample trivialGen
