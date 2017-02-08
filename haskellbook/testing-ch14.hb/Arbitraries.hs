3893b550a0c1adbae723c830995466072cdf35b7
Galder Zamarreño <galder@zamarreno.com>
2017-02-08 16:03:44 +0100

    [HB] CH14 - Identity arbitrary

--- haskellbook/testing-ch14.hb/Arbitraries.hs
+++ haskellbook/testing-ch14.hb/Arbitraries.hs
@@ -18,6 +18,27 @@ instance Arbitrary Trivial where
         trivialGen
 
 
+data Identity a =
+    Identity a
+    deriving (Eq, Show)
+
+
+identityGen :: Arbitrary a => Gen (Identity a)
+identityGen =
+    do  a <- arbitrary
+        return (Identity a)
+
+
+instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = identityGen
+
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt =
+    identityGen
+
+
 main :: IO ()
 main =
-    sample trivialGen
+    sample identityGenInt
+    -- sample trivialGen
