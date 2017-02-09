e525699975298a1b25fd819e3504f0f24b95b221
Galder Zamarreño <galder@zamarreno.com>
2017-02-09 09:54:05 +0100

    [HB] CH14 - List sort quick check

--- haskellbook/testing-ch14.hb/UsingQuickCheck.hs
+++ haskellbook/testing-ch14.hb/UsingQuickCheck.hs
@@ -1,6 +1,7 @@
 module UsingQuickCheck where
 
 import Test.QuickCheck
+import Data.List (sort)
 
 
 half :: Fractional a => a -> a
@@ -18,6 +19,24 @@ prop_half x =
     halfIdentity x == x
 
 
+-- for any list you apply sort to
+-- this property should hold
+listOrdered :: (Ord a) => [a] -> Bool
+listOrdered xs =
+    snd $ foldr go (Nothing, True) xs
+    where   go _ status@(_, False) =
+                status
+            go y (Nothing, t) =
+                (Just y, t)
+            go y (Just x, _) =
+                (Just y, x >= y)
+
+
+prop_listSort :: [Int] -> Bool
+prop_listSort = listOrdered . sort
+
+
 main :: IO ()
 main =
-    quickCheck prop_half
+    do  quickCheck prop_half
+        quickCheck prop_listSort
