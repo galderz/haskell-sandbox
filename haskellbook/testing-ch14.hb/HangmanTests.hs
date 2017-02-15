3f578914c80c2ff8bc82b9a9a39ee3e731ba7b59
Galder Zamarreño <galder@zamarreno.com>
2017-02-15 16:34:44 +0100

    [HB] CH14 - Basic Hangman puzzle test

---
+++ haskellbook/testing-ch14.hb/HangmanTests.hs
@@ -0,0 +1,36 @@
+module HangmanTests where
+
+
+import Test.Hspec
+
+
+data Puzzle =
+    Puzzle String [Maybe Char] [Char]
+    deriving (Eq, Show)
+
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle x =
+  Puzzle x (map (const Nothing) x) []
+
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar s) c =
+  Puzzle word newFilledInSoFar newGuessed
+  where zipper guessed wordChar guessChar =
+          if wordChar == guessed
+          then Just wordChar
+          else guessChar
+        newFilledInSoFar =
+          zipWith (zipper c) word filledInSoFar
+        newGuessed =
+          if elem c word
+          then s
+          else c : s
+
+
+main :: IO ()
+main = hspec $ do
+  describe "Fresh puzzle" $ do
+    it "can be used to create a Puzzle" $ do
+      freshPuzzle "haskell" `shouldBe` (Puzzle "haskell" (replicate 7 Nothing) "")
