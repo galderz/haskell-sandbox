d7463071d98235678d002d18d16f55c2dc854536
Galder Zamarreño <galder@zamarreno.com>
2017-02-16 17:15:36 +0100

    [HB] CH14 - Add vigenere function, need unvigenere

--- haskellbook/testing-ch14.hb/ValidatingCiphers.hs
+++ haskellbook/testing-ch14.hb/ValidatingCiphers.hs
@@ -71,7 +71,39 @@ prop_CaesarUpper =
         (\(s, n) -> caesarIdentity s n)
 
 
+vigenere :: String -> String -> String
+vigenere "" _ =
+  ""
+vigenere phr kw =
+  couple phr kw kw
+
+
+couple :: String -> String -> String -> [Char]
+couple "" _ w =
+  []
+couple msg "" w =
+  couple msg w w
+couple (' ':xs) ks w =
+  ' ' : couple xs ks w
+couple (x:xs) (k:ks) w =
+   ch : couple xs ks w
+   where ch = rightShift x (ordy)
+         ordx = ord x
+         ordy = (ord k) - 65
+
+
+rightShift :: Char -> Int -> Char
+rightShift x n =
+    chr (i + chAt)
+    where chAt = mod ((ord x) + n - i) 26
+          i = ordFst x
+          ordFst ch
+            | isUpper ch = 65
+            | otherwise = 97
+
+
 main :: IO ()
 main =
     do  quickCheck prop_CaesarLower
         quickCheck prop_CaesarUpper
+        -- quickCheck prop_VigenereLower
