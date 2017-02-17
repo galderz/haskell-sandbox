fde273dd4f6a635c9792a9deb057c7743edbd1cb
Galder Zamarreño <galder@zamarreno.com>
2017-02-17 12:22:12 +0100

    [HB] CH15 - Basic empty monoid rule

--- haskellbook/monoid-ch15.hb/Monoids.hs
+++ haskellbook/monoid-ch15.hb/Monoids.hs
@@ -2,6 +2,13 @@ module Monoids where
 
 
 -- basic monoid rule
+basicMonoidRule :: Bool
 basicMonoidRule =
-    mappend [1..5] [] ==
-        mappend [] [1..5]
+    mappend [1..5] []
+        == mappend [] [1..5]
+
+
+basicEmptyMonoidRule :: (Monoid a, Eq a) => a -> Bool
+basicEmptyMonoidRule x =
+    mappend x mempty
+        == mappend mempty x
