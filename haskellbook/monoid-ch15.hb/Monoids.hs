84c31adc33e3e6b331caffe88b12b46e043a6a8e
Galder Zamarreño <galder@zamarreno.com>
2017-02-17 12:27:14 +0100

    [HB] CH15 - List append as a monoid

--- haskellbook/monoid-ch15.hb/Monoids.hs
+++ haskellbook/monoid-ch15.hb/Monoids.hs
@@ -12,3 +12,9 @@ basicEmptyMonoidRule :: (Monoid a, Eq a) => a -> Bool
 basicEmptyMonoidRule x =
     mappend x mempty
         == mappend mempty x
+
+
+listAppendMonoid :: Bool
+listAppendMonoid =
+    mappend [1,2,3] [4,5,6]
+        == (++) [1,2,3] [4,5,6]
