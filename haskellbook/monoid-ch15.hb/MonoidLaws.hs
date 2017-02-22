40829a3f54a3aea4011870fb46f5c5d48852f630
Galder Zamarreño <galder@zamarreno.com>
2017-02-22 17:22:00 +0100

    [HB] CH15 - Monoid law associativity

--- haskellbook/monoid-ch15.hb/MonoidLaws.hs
+++ haskellbook/monoid-ch15.hb/MonoidLaws.hs
@@ -9,3 +9,8 @@ leftIdentity =
 
 rightIdentity =
     mappend (Sum 1) mempty == (Sum 1)
+
+
+associativity =
+    mappend (Sum 1) (mappend (Sum 2) (Sum 3)) ==
+        mappend (mappend (Sum 1) (Sum 2)) (Sum 3)
