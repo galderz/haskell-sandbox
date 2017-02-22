c28ccdd9680c2dd2f12b911dfb77548c4a33f0c7
Galder Zamarreño <galder@zamarreno.com>
2017-02-22 17:24:51 +0100

    [HB] CH15 - Associativity law with infix operator

--- haskellbook/monoid-ch15.hb/MonoidLaws.hs
+++ haskellbook/monoid-ch15.hb/MonoidLaws.hs
@@ -3,14 +3,19 @@ module MonoidLaws where
 import Data.Monoid
 
 
-leftIdentity =
+leftIdentitySum =
     mappend mempty (Sum 1) == (Sum 1)
 
 
-rightIdentity =
+rightIdentitySum =
     mappend (Sum 1) mempty == (Sum 1)
 
 
-associativity =
+associativitySum =
     mappend (Sum 1) (mappend (Sum 2) (Sum 3)) ==
         mappend (mappend (Sum 1) (Sum 2)) (Sum 3)
+
+
+associativitySum' =
+    (Sum 1) <> (Sum 2 <> Sum 3) ==
+        (Sum 1 <> Sum 2) <> Sum 3
