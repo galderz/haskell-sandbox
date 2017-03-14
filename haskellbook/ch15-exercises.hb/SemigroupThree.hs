72cbe6a1fe81b05a4c16f3f138c7de3cd26e45e7
Galder Zamarreño <galder@zamarreno.com>
2017-03-14 13:30:32 +0100

    [HB] CH15 - Semigroup Three chapter exercise

---
+++ haskellbook/ch15-exercises.hb/SemigroupThree.hs
@@ -0,0 +1,40 @@
+module SemigroupTwo where
+
+import Data.Semigroup
+import Test.QuickCheck
+
+
+data Three a b c =
+    Three a b c
+    deriving (Eq, Show)
+
+
+instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
+    Three a b c <> Three a' b' c' =
+        Three (a <> a') (b <> b') (c <> c')
+
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+    arbitrary = do
+        x <- arbitrary
+        y <- arbitrary
+        z <- arbitrary
+        return $ Three x y z
+
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c =
+    (a <> (b <> c))
+        == ((a <> b) <> c)
+
+
+type ThreeAssoc =
+    Three String Ordering String
+    -> Three String Ordering String
+    -> Three String Ordering String
+    -> Bool
+
+
+main :: IO ()
+main =
+    quickCheck (semigroupAssoc :: ThreeAssoc)
