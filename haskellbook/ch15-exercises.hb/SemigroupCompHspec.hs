00aeee84bfa30f940281e18a5ec9b42c25245cad
Galder Zamarreño <galder@zamarreno.com>
2017-03-16 16:29:17 +0100

    [HB] CH15 - Semigroup Comp w/ Hspec

---
+++ haskellbook/ch15-exercises.hb/SemigroupCompHspec.hs
@@ -0,0 +1,38 @@
+module SemigroupCompHspec where
+
+import Data.Semigroup (Semigroup, (<>), Sum(Sum, getSum))
+import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Function
+
+
+newtype Comp a =
+    Comp { unComp :: (a -> a) }
+
+
+instance Semigroup (Comp a) where
+    Comp f <> Comp g =
+        Comp (g . f)
+
+
+f :: Comp Integer
+f =
+    Comp $ \n -> n + 1
+
+
+g :: Comp Integer
+g =
+    Comp $ \n -> n - 1
+
+
+main :: IO ()
+main = hspec $ do
+    describe "Comp" $ do
+        it "unComp applied on (-1) and (+1) on 0 is equal to 0" $ do
+            (unComp (f <> g) $ 0) `shouldBe` 0
+        it "unComp applied on (-1) and (+1) on 1 is equal to 1" $ do
+            (unComp (f <> g) $ 1) `shouldBe` 1
+        it "unComp applied on (+1) and (+1) on 1 is equal to 3" $ do
+            (unComp (f <> f) $ 1) `shouldBe` 3
+        it "unComp applied on (+1) and (-1) on 1 is equal to 1" $ do
+            (unComp (g <> f) $ 1) `shouldBe` 1
