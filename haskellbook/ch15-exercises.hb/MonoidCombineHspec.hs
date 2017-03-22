ae98db44844d1aa8bbb225f4462db4bf2995e1a8
Galder Zamarreño <galder@zamarreno.com>
2017-03-22 11:01:17 +0100

    [HB] CH15 - Monoid Combine w/ Hspec chapter ex

---
+++ haskellbook/ch15-exercises.hb/MonoidCombineHspec.hs
@@ -0,0 +1,46 @@
+module MonoidCombineHspec where
+
+import Data.Semigroup (Semigroup, (<>), Sum(Sum, getSum))
+import Test.Hspec
+import Test.QuickCheck (Arbitrary, arbitrary, quickCheck, elements)
+
+
+newtype Combine a b =
+    Combine { unCombine :: (a -> b) }
+
+
+instance Semigroup b => Semigroup (Combine a b) where
+    Combine f <> Combine f' =
+        Combine (f <> f')
+
+
+instance (Semigroup b, Monoid b) => Monoid (Combine a b) where
+    mempty =
+        Combine mempty
+
+    mappend = (<>)
+
+
+f :: Combine Integer (Sum Integer)
+f =
+    Combine $ \n -> Sum (n + 1)
+
+
+g :: Combine Integer (Sum Integer)
+g =
+    Combine $ \n -> Sum (n - 1)
+
+
+main :: IO ()
+main = hspec $ do
+    describe "Comp" $ do
+        it "unCombine applied on (-1) and (+1) on 0 is equal to Sum(0)" $ do
+            (unCombine (f <> g) $ 0) `shouldBe` Sum 0 -- (0 - 1) <> (0 + 1)
+        it "unCombine applied on (-1) and (+1) on 1 is equal to Sum(2)" $ do
+            (unCombine (f <> g) $ 1) `shouldBe` Sum 2 -- (1 - 1) <> (1 + 1)
+        it "unCombine applied on (+1) and (+1) on 1 is equal to Sum(4)" $ do
+            (unCombine (f <> f) $ 1) `shouldBe` Sum 4 -- (1 + 1) <> (1 + 1)
+        it "unCombine applied on (+1) and (-1) on 1 is equal to Sum (2)" $ do
+            (unCombine (g <> f) $ 1) `shouldBe` Sum 2 -- (1 + 1) <> (1 - 1)
+        it "unCombine applied on (+1) on 1 with mempty is equal to Sum (2)" $ do
+            (unCombine (mappend f mempty) $ 1) `shouldBe` Sum 2
