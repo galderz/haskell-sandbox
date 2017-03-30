d099b57e80ba2ad5940e82d4ed215f7d875aa538
Galder Zamarreño <galder@zamarreno.com>
2017-03-30 15:39:42 +0200

    [HB] CH16 - Functor whose id law is broken

--- haskellbook/functor-ch16.hb/FunctorLaws.hs
+++ haskellbook/functor-ch16.hb/FunctorLaws.hs
@@ -7,3 +7,29 @@ idLaw =
 
 compositionLaw =
     fmap ((+1) . (*2)) [1..5] == (fmap (+1) . fmap (*2) $ [1..5])
+
+
+data WhoCares a =
+    ItDoesnt
+    | Matter a
+    | WhatThisIsCalled
+    deriving (Eq, Show)
+
+
+-- Law breaking Functor
+instance Functor WhoCares where
+    fmap _ ItDoesnt =
+        WhatThisIsCalled
+
+    fmap _ WhatThisIsCalled =
+        ItDoesnt
+
+    fmap f (Matter a) =
+        Matter (f a)
+
+
+-- Check how identity law is broken by functor above
+idLawBreak :: Bool
+idLawBreak =
+    fmap id (ItDoesnt :: WhoCares String) == id (ItDoesnt :: WhoCares String)
+    -- fmap id ItDoesnt == id ItDoesnt
