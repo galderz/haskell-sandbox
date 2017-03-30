bd37436e2d7a940c21b33fde06cd7918d78bd54f
Galder Zamarreño <galder@zamarreno.com>
2017-03-30 16:01:29 +0200

    [HB] CH16 - More Functors

--- haskellbook/functor-ch16.hb/Functors.hs
+++ haskellbook/functor-ch16.hb/Functors.hs
@@ -24,3 +24,51 @@ fmapOnTuple =
 
 fmapOnEither =
     fmap (++ ", Esq.") (Right "Chris Allen")
+
+
+replaceWithP =
+    const 'p' -- const :: a -> b -> a
+
+
+rwpJust =
+    fmap replaceWithP (Just 10) -- Just 'p'
+
+
+rwpNothing =
+    fmap replaceWithP Nothing -- Nothing
+
+
+rwpIntList =
+    fmap replaceWithP [1, 2, 3, 4, 5] -- "ppppp"
+
+
+rwpString =
+    fmap replaceWithP "Ave" -- "ppp"
+
+
+rwpEmpty =
+    fmap replaceWithP [] -- ""
+
+
+rwpTupleInt =
+    fmap replaceWithP (10, 20) -- "(10, 'p')"
+
+
+rwpTupleString =
+    fmap replaceWithP (10, "woo") -- "(10, 'p')"
+
+
+tossEmOne =
+    fmap (+1) negate
+
+
+tossEmOne10 =
+    tossEmOne 10 -- -9
+
+
+tossEmOneMinus10 =
+    tossEmOne (-10) -- 11
+
+
+tossEmOne' =
+    (+1) . negate -- tossEmOne' 10 // tossEmOne' (-10) => same results as above
