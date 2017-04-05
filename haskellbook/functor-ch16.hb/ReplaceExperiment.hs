fef588fa02288fedf067131cdeb5b48aa5babbc6
Galder Zamarreño <galder@zamarreno.com>
2017-04-05 16:03:52 +0200

    [HB] CH16 - Twice lifting replace experiment

--- haskellbook/functor-ch16.hb/ReplaceExperiment.hs
+++ haskellbook/functor-ch16.hb/ReplaceExperiment.hs
@@ -26,3 +26,19 @@ liftedReplace =
 liftedReplace' :: [Maybe [Char]] -> [Char]
 liftedReplace' =
     liftedReplace
+
+
+
+-- Prelude> :t (fmap . fmap) replaceWithP
+-- (fmap . fmap) replaceWithP
+--   ::(Functor f1, Functor f) => f (f1 a) -> f (f1 Char)
+twiceLifted :: (Functor f1, Functor f) => f (f1 a) -> f (f1 Char)
+twiceLifted =
+    (fmap . fmap) replaceWithP
+
+
+-- f ~ []
+-- f1 ~ Maybe
+twiceLifted' :: [Maybe [Char]] -> [Maybe Char]
+twiceLifted' =
+    twiceLifted
