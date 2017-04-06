7d9364b002328704a0dd554e8cbcdee7b9d65846
Galder Zamarreño <galder@zamarreno.com>
2017-04-06 19:28:58 +0200

    [HB] CH16 - Heavy lifting ex. 5

--- haskellbook/functor-ch16.hb/HeavyLifting.hs
+++ haskellbook/functor-ch16.hb/HeavyLifting.hs
@@ -19,3 +19,13 @@ c =
 -- d 0 => "1[0,1,2,3]"
 d =
     ((return '1' ++) . show) . (\x -> [x, 1..3])
+
+
+-- 3693
+e :: IO Integer
+e =
+    let
+        ioi = readIO "1" :: IO Integer
+        changed = fmap (read . ("123"++) . show) ioi
+    in
+        fmap (*3) changed
