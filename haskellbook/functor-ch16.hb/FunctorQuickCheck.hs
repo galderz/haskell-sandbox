428e3a560e1afdb29eab83fe094a58929f8a1513
Galder Zamarreño <galder@zamarreno.com>
2017-04-07 17:49:54 +0200

    [HB] CH16 - QuickCheck Functor generated functions

--- haskellbook/functor-ch16.hb/FunctorQuickCheck.hs
+++ haskellbook/functor-ch16.hb/FunctorQuickCheck.hs
@@ -1,6 +1,7 @@
 module FunctorQuickCheck where
 
 import Test.QuickCheck
+import Test.QuickCheck.Function
 
 
 functorIdentity :: (Functor f, Eq (f a)) => f a -> Bool
@@ -9,11 +10,23 @@ functorIdentity f =
 
 
 functorCompose :: (Eq (f c), Functor f) => (a -> b) -> (b -> c) -> f a -> Bool
-functorCompose f g x=
+functorCompose f g x =
     (fmap g (fmap f x)) == (fmap (g . f) x)
+
+
+functorCompose' :: (Eq (f c), Functor f) => f a -> Fun a b -> Fun b c -> Bool
+functorCompose' x (Fun _ f) (Fun _ g) =
+    (fmap g (fmap f x)) == (fmap (g . f) x)
+
+
+type IntToInt = Fun Int Int
+
+
+type IntFC = [Int] -> IntToInt -> IntToInt -> Bool
 
 
 main :: IO ()
 main =
     do  quickCheck $ \x -> functorIdentity (x :: [Int])
         quickCheck $ \x -> functorCompose (+1) (*2) (x :: [Int])
+        quickCheck (functorCompose' :: IntFC)
