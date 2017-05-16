6eab2c084cabe19dd4975d4afedd455fd5986a81
Galder Zamarreño <galder@zamarreno.com>
2017-05-16 20:03:29 +0200

    [HB] CH17 - Make person

--- haskellbook/applicative-ch17.hb/MaybeApplicative.hs
+++ haskellbook/applicative-ch17.hb/MaybeApplicative.hs
@@ -5,6 +5,45 @@ validateLength maxLen s =
     then Nothing
     else Just s
 
+
+newtype Name =
+    Name String
+    deriving (Eq, Show)
+
+
+newtype Address =
+    Address String
+    deriving (Eq, Show)
+
+
+mkName :: String -> Maybe Name
+mkName s =
+    fmap Name $ validateLength 25 s
+
+
+mkAddress :: String -> Maybe Address
+mkAddress a =
+    fmap Address $ validateLength 100 a
+
+
+data Person =
+    Person Name Address
+    deriving (Eq, Show)
+
+
+mkPerson :: String -> String -> Maybe Person
+mkPerson n a =
+    case mkName n of
+        Nothing ->
+            Nothing
+        Just n' ->
+            case mkAddress a of
+                Nothing ->
+                    Nothing
+                Just a' ->
+                    Just $ Person n' a'
+
+
 main :: IO ()
 main =
     do  print $ validateLength 5 "basel"
