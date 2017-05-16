2202efab9dcc72fbd2ae18afe7aef633a8071988
Galder Zamarreño <galder@zamarreno.com>
2017-05-16 19:44:34 +0200

    [HB] CH17 - Constant applicative

--- haskellbook/applicative-ch17.hb/ConstantApplicative.hs
+++ haskellbook/applicative-ch17.hb/ConstantApplicative.hs
@@ -1,3 +1,6 @@
+import Control.Applicative
+import Data.Monoid
+
 
 newtype Constant a b =
     Constant { getConstant :: a }
@@ -9,6 +12,16 @@ instance Functor (Constant a) where
         Constant x
 
 
+instance Monoid a => Applicative (Constant a) where
+    pure _ =
+        Constant mempty
+    (<*>) (Constant f) (Constant y) =
+        Constant (mappend f y)
+
+
 main :: IO ()
 main =
     do  print $ fmap (+1) (Constant 5)
+        print $ Constant (Sum 1) <*> Constant (Sum 2)
+        -- print $ Constant undefined <*> Constant (Sum 2)
+        print $ (pure 1 :: Constant String Int)
