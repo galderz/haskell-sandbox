6bd36a773f10f811d5f9c91af61f6fd292445874
Galder Zamarreño <galder@zamarreno.com>
2017-05-18 18:00:01 +0200

    [HB] CH17 - id, fmap id and pure id are same

--- haskellbook/applicative-ch17.hb/ApplicativeLaws.hs
+++ haskellbook/applicative-ch17.hb/ApplicativeLaws.hs
@@ -7,3 +7,6 @@ main =
         -- print $ pure id <*> Left "Erro'ish"
         -- print $ pure id <*> Right 8001
         print $ pure id <*> (+1) $ 2
+        print $ id [1..5] == fmap id [1..5]
+        print $ fmap id [1..5] == (pure id <*> [1..5])
+        print $ (pure id <*> [1..5]) == id [1..5]
