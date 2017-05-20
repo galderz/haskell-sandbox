fe0a6772a6ca26836ad254934911facd03d26d00
Galder Zamarreño <galder@zamarreno.com>
2017-05-20 19:47:37 +0200

    [HB] CH17 - Incorrect ZipListMonoid, [] is zero

--- haskellbook/applicative-ch17.hb/ZipListMonoid.hs
+++ haskellbook/applicative-ch17.hb/ZipListMonoid.hs
@@ -1,6 +1,29 @@
+module ZipListMonoid where
+
+import Control.Applicative
 import Data.Monoid
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+
+-- ZipList [] is Zero, not identity:
+-- Zero
+-- n * 0 == 0
+-- Identity
+-- n * 1 == n
+instance Monoid a => Monoid (ZipList a) where
+    mempty =
+        ZipList []
+    mappend =
+        liftA2 mappend
+
+
+instance Eq a => EqProp (ZipList a) where
+    (=-=) =
+        eq
 
 
 main :: IO ()
 main =
-    print $ 1 <> (2 :: Sum Integer)
+    quickBatch $ monoid (ZipList [1 :: Sum Int])
