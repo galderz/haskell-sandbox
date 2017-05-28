fb0c6b769385f51a475c7af04626e0aa216514d8
Galder Zamarreño <galder@zamarreno.com>
2017-05-28 16:21:10 +0200

    [HB] CH17 - Validation functor w/ QC

--- haskellbook/applicative-ch17.hb/EitherVsValidation.hs
+++ haskellbook/applicative-ch17.hb/EitherVsValidation.hs
@@ -1,3 +1,35 @@
+module EitherVsValidation where
+
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+
+data Validation e a =
+    Failure' e
+    | Success' a
+    deriving (Eq, Show)
+
+
+instance Functor (Validation e) where
+    fmap _ (Failure' e) =
+        Failure' e
+    fmap f (Success' a) =
+        Success' (f a)
+
+
+instance (Arbitrary e, Arbitrary a) => Arbitrary (Validation e a) where
+    arbitrary =
+        do  x <- arbitrary
+            y <- arbitrary
+            oneof [ return $ Failure' x
+                    , return $ Success' y
+                ]
+
+
+instance (Eq e, Eq a) => EqProp (Validation e a) where
+    (=-=) =
+        eq
 
 
 main :: IO ()
@@ -7,3 +39,4 @@ main =
         print $ Right (+1) <*> Left ":("
         print ((Left ":("  <*> Right 1) :: Either String Int)
         print ((Left ":(" <*> Left "sadface.png") :: Either String Int)
+        quickBatch $ functor (undefined :: Validation (String, String, Int) (String, String, Int))
