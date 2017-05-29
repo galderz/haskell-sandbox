8d2bc349849a78f36d01a9e440e95235569651b1
Galder Zamarreño <galder@zamarreno.com>
2017-05-29 15:57:21 +0200

    [HB] CH17 - Chapter exercise IO specialization

--- haskellbook/applicative-ch17.hb/SpecializeTypes.hs
+++ haskellbook/applicative-ch17.hb/SpecializeTypes.hs
@@ -1,5 +1,9 @@
+import Data.Char
 
 main :: IO ()
 main =
     do  print ((pure 4) :: [Int])
         print ([(+1)] <*> [4])
+        pure 'a' :: IO Char
+        (pure ord) <*> (pure 'a' :: IO Char)
+        print ""
