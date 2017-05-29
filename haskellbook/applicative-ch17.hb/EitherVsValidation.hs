e7869769b0c5e729ad6a3a95bb085c61f15b7346
Galder Zamarreño <galder@zamarreno.com>
2017-05-29 14:11:31 +0200

    [HB] CH17 - Validation applicative w/ QC

--- haskellbook/applicative-ch17.hb/EitherVsValidation.hs
+++ haskellbook/applicative-ch17.hb/EitherVsValidation.hs
@@ -1,35 +1,52 @@
 module EitherVsValidation where
 
-import Test.QuickCheck
-import Test.QuickCheck.Checkers
-import Test.QuickCheck.Classes
-
+import Test.QuickCheck (Arbitrary, arbitrary, oneof)
+import Test.QuickCheck.Checkers (quickBatch, eq, (=-=), EqProp, TestBatch)
+import Test.QuickCheck.Classes (applicative, functor, monoid)
 
 data Validation e a =
-    Failure' e
-    | Success' a
+    Failure e
+    | Success a
     deriving (Eq, Show)
 
 
 instance Functor (Validation e) where
-    fmap _ (Failure' e) =
-        Failure' e
-    fmap f (Success' a) =
-        Success' (f a)
+    fmap _ (Failure e) =
+        Failure e
+    fmap f (Success a) =
+        Success (f a)
+
+
+-- This is different
+instance Monoid e => Applicative (Validation e) where
+    pure x =
+        Success x
+    (<*>) (Failure f) (Failure x) =
+        Failure (mappend f x)
+    (<*>) _ (Failure x) =
+        Failure x
+    (<*>) (Failure x) _ =
+        Failure x
+    (<*>) (Success f) (Success y) =
+        Success (f y)
 
 
 instance (Arbitrary e, Arbitrary a) => Arbitrary (Validation e a) where
     arbitrary =
         do  x <- arbitrary
             y <- arbitrary
-            oneof [ return $ Failure' x
-                    , return $ Success' y
+            oneof [ return $ Failure x
+                    , return $ Success y
                 ]
 
 
 instance (Eq e, Eq a) => EqProp (Validation e a) where
     (=-=) =
         eq
+
+
+v =
+    undefined :: Validation (String, String, [Int]) (String, String, [Int])
 
 
 main :: IO ()
@@ -39,4 +56,5 @@ main =
         print $ Right (+1) <*> Left ":("
         print ((Left ":("  <*> Right 1) :: Either String Int)
         print ((Left ":(" <*> Left "sadface.png") :: Either String Int)
-        quickBatch $ functor (undefined :: Validation (String, String, Int) (String, String, Int))
+        quickBatch $ functor v
+        quickBatch $ applicative v
