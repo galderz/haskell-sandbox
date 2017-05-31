40113c1fc7f2b760d2ac39fe592f419bf28a5491
Galder Zamarreño <galder@zamarreno.com>
2017-05-31 14:41:10 +0200

    [HB] CH17 - Functor for Pair

---
+++ haskellbook/applicative-ch17.hb/Applicatives.hs
@@ -0,0 +1,40 @@
+module Applicatives where
+
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+
+data Pair a =
+    Pair a a
+    deriving (Eq, Show)
+
+
+instance Functor Pair where
+    fmap f (Pair x y) =
+        Pair (f x) (f y)
+
+
+instance Applicative Pair where
+    pure =
+        undefined
+    (<*>) =
+        undefined
+
+
+instance Arbitrary a => Arbitrary (Pair a) where
+    arbitrary =
+        do  x <- arbitrary
+            y <- arbitrary
+            return $ Pair x y
+
+
+instance Eq a => EqProp (Pair a) where
+    (=-=) =
+        eq
+
+
+main :: IO ()
+main =
+    quickBatch $ functor (undefined :: Pair (String, String, Int))
+    -- quickBatch $ applicative (undefined :: Pair (String, String, Int))
