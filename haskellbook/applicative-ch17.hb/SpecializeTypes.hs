20ed8193c9e57ee34daa105628646287e103f2e1
Galder Zamarreño <galder@zamarreno.com>
2017-05-31 14:27:04 +0200

    [HB] CH17 - Chapter exercise (->) specialization

--- haskellbook/applicative-ch17.hb/SpecializeTypes.hs
+++ haskellbook/applicative-ch17.hb/SpecializeTypes.hs
@@ -1,5 +1,14 @@
 import Data.Char
 
+f :: Int -> Int
+f =
+    pure 4
+
+f' :: Int -> Int
+f' =
+    pure (+1) <*> pure 4
+
+
 main :: IO ()
 main =
     do  print ((pure 4) :: [Int]) -- []
@@ -8,3 +17,7 @@ main =
         (pure ord) <*> (pure 'a' :: IO Char)
         print ((pure 4) :: (String, Int)) -- (,) a
         print ((("boo", (+1)) <*> ("ahh", 4)))
+        -- print ((pure 4) :: (Int -> Int))
+        -- print ((pure (+1) <*> 4) :: (Int -> Int))
+        print $ f 5
+        print $ f' 8
