13692ca2df8074444e83153ba9562e27fe1a91f2
Galder Zamarreño <galder@zamarreno.com>
2017-06-05 17:16:40 +0200

    [HB] CH18 - Bind in terms of fmap and join

--- haskellbook/monad-ch18.hb/WhatIsMonad.hs
+++ haskellbook/monad-ch18.hb/WhatIsMonad.hs
@@ -1,6 +1,13 @@
+import Control.Monad (join)
 
 andOne x =
     [x, 1]
+
+
+-- keep in mind this is (>>=) flipped
+bind :: Monad m => (a -> m b) -> m a -> m b
+bind f ma =
+    join $ fmap f ma
 
 
 main :: IO ()
