79351da3ab59c2a19b2678278230be620bfd30df
Galder Zamarreño <galder@zamarreno.com>
2017-06-05 17:12:30 +0200

    [HB] CH18 - Monad is a generalization of concat

--- haskellbook/monad-ch18.hb/WhatIsMonad.hs
+++ haskellbook/monad-ch18.hb/WhatIsMonad.hs
@@ -1,7 +1,15 @@
 
+andOne x =
+    [x, 1]
+
+
 main :: IO ()
 main =
     do  -- You can derive Functor in terms of Monad
         -- fmap f xs = xs >>= return . f
         print $ fmap (+1) [1..3]
         print $ [1..3] >>= return . (+1)
+        -- Monad, in a sense, is a generalization of concat
+        print $ andOne 10
+        print $ fmap andOne [4, 5, 6]
+        print $ concat $ fmap andOne [4, 5, 6]
