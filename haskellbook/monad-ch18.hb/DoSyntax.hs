87fefd7a61b9b7ab09f3c40ea2d97fc98dd19618
Galder Zamarreño <galder@zamarreno.com>
2017-06-07 16:53:20 +0200

    [HB] CH18 - Sequencing alternatives

--- haskellbook/monad-ch18.hb/DoSyntax.hs
+++ haskellbook/monad-ch18.hb/DoSyntax.hs
@@ -4,3 +4,15 @@ sequencing :: IO ()
 sequencing =
     do  putStrLn "blah"
         putStrLn "another thing"
+
+
+sequencing' :: IO()
+sequencing' =
+    putStrLn "blah" >>
+    putStrLn "another thing"
+
+
+sequencing'' :: IO ()
+sequencing'' =
+    putStrLn "blah" *>
+    putStrLn "another thing"
