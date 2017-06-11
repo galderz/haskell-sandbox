3a666b71076af0563229a0a24b6ab371d781d679
Galder Zamarreño <galder@zamarreno.com>
2017-06-11 13:50:23 +0200

    [HB] CH18 - Verify Sum functor

--- haskellbook/monad-ch18.hb/SumMonad.hs
+++ haskellbook/monad-ch18.hb/SumMonad.hs
@@ -1,5 +1,9 @@
 module SumMonad where
 
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
 
 data Sum a b =
     First a
@@ -10,5 +14,22 @@ data Sum a b =
 instance Functor (Sum a) where
     fmap _ (First a) =
         First a
-    fmap f (Second b) =
-        Second (f b)
+    fmap f (Second x) =
+        Second (f x)
+
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Sum a b) where
+    arbitrary =
+        do  x <- arbitrary
+            y <- arbitrary
+            oneof [return $ First x, return $ Second y]
+
+
+instance (Eq a, Eq b) => EqProp (Sum a b) where
+    (=-=) =
+        eq
+
+
+main :: IO ()
+main =
+    quickBatch $ functor (undefined :: Sum (String, String, Int) (String, String, Int))
