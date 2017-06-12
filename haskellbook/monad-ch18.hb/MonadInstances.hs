97a2d67f2e1942cada67f03f556735a6b7f9d995
Galder Zamarreño <galder@zamarreno.com>
2017-06-12 15:46:24 +0200

    [HB] CH18 - Custom Either functor

--- haskellbook/monad-ch18.hb/MonadInstances.hs
+++ haskellbook/monad-ch18.hb/MonadInstances.hs
@@ -10,9 +10,22 @@ data Nope a =
     deriving (Eq, Show)
 
 
+data PhhhbbtttEither b a =
+    Lft a
+    | Rght b
+    deriving (Eq, Show)
+
+
 instance Functor Nope where
     fmap _ _ =
         NopeDotJpg
+
+
+instance Functor (PhhhbbtttEither b) where
+    fmap f (Lft x) =
+        Lft (f x)
+    fmap _ (Rght b) =
+        Rght b
 
 
 instance Applicative Nope where
@@ -34,7 +47,19 @@ instance Arbitrary (Nope a) where
         return NopeDotJpg
 
 
+instance (Arbitrary b, Arbitrary a) => Arbitrary (PhhhbbtttEither b a) where
+    arbitrary =
+        do  x <- arbitrary
+            y <- arbitrary
+            oneof [return $ Lft x, return $ Rght y]
+
+
 instance (Eq a) => EqProp (Nope a) where
+    (=-=) =
+        eq
+
+
+instance (Eq b, Eq a) => EqProp (PhhhbbtttEither b a) where
     (=-=) =
         eq
 
@@ -43,7 +68,10 @@ main :: IO ()
 main =
     do
         let
-            testNode = undefined :: Nope (String, String, [Int])
-        quickBatch $ functor testNode
-        quickBatch $ applicative testNode
-        quickBatch $ monad testNode
+            testNope = undefined :: Nope (String, String, [Int])
+            testEither = undefined :: PhhhbbtttEither
+                (String, String, [Int]) (String, String, [Int])
+        -- quickBatch $ functor testNope
+        -- quickBatch $ applicative testNope
+        quickBatch $ monad testNope
+        quickBatch $ functor testEither
