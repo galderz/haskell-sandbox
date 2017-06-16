e6042aa27ca5ee0f2476ed6ba55779ed8686a1b9
Galder Zamarreño <galder@zamarreno.com>
2017-06-16 14:09:19 +0200

    [HB] CH18 - l1 function exercise

--- haskellbook/monad-ch18.hb/MonadExamples.hs
+++ haskellbook/monad-ch18.hb/MonadExamples.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 import Control.Monad (join)
 
 
@@ -6,9 +7,16 @@ j =
     join
 
 
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 f m =
+    fmap f m
+
+
 main :: IO ()
 main =
     do  print $ j [[1, 2], [], [3]]
         print $ j (Just (Just 1))
         print $ j ((Just Nothing) :: Maybe (Maybe Int))
         print $ j (Nothing :: Maybe (Maybe Int))
+        print $ l1 (+1) (Just 1)
+        print $ l1 (+1) Nothing
