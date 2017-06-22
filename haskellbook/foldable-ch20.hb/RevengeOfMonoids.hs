f2004c31d7c8537acceb323a0e8d980a1749531b
Galder Zamarreño <galder@zamarreno.com>
2017-06-22 13:41:47 +0200

    [HB] CH20 - Let compiler identity Monoid to use

--- haskellbook/foldable-ch20.hb/RevengeOfMonoids.hs
+++ haskellbook/foldable-ch20.hb/RevengeOfMonoids.hs
@@ -8,3 +8,5 @@ main =
         print $ fold [Sum 1, Sum 2, Sum 3, Sum 4, Sum 5]
         print $ fold [1, 2, 3, 4, 5 :: Sum Integer]
         print $ fold [1, 2, 3, 4, 5 :: Product Integer]
+        print $ foldr (++) "" ["hello", "julie"]
+        print $ fold ["hello", "julie"]
