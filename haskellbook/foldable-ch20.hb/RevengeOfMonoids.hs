1b3597b61693ba44c9b2d5a30bf7d39370762739
Galder Zamarreño <galder@zamarreno.com>
2017-06-22 13:40:16 +0200

    [HB] CH20 - Selecting monoid for Foldable

--- haskellbook/foldable-ch20.hb/RevengeOfMonoids.hs
+++ haskellbook/foldable-ch20.hb/RevengeOfMonoids.hs
@@ -6,3 +6,5 @@ main :: IO ()
 main =
     do  print $ foldr (+) 0 [1..5]
         print $ fold [Sum 1, Sum 2, Sum 3, Sum 4, Sum 5]
+        print $ fold [1, 2, 3, 4, 5 :: Sum Integer]
+        print $ fold [1, 2, 3, 4, 5 :: Product Integer]
