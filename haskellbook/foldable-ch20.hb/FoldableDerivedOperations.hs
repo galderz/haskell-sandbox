2b2837f6aa491b2a2f1a7b6711b805d229d8272e
Galder Zamarreño <galder@zamarreno.com>
2017-06-23 18:18:11 +0200

    [HB] CH20 - Foldable concatMap and toList

--- haskellbook/foldable-ch20.hb/FoldableDerivedOperations.hs
+++ haskellbook/foldable-ch20.hb/FoldableDerivedOperations.hs
@@ -6,3 +6,6 @@ main :: IO ()
 main =
     do  print $ toList (Just 1)
         print $ map toList [Just 1, Just 2, Just 3]
+        print $ concatMap toList [Just 1, Just 2, Just 3]
+        print $ concatMap toList [Just 1, Just 2, Nothing]
+        print $ toList (1, 2)
