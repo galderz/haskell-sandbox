1ffa150479af76340267dc9549b6b6ffb13a68a8
Galder Zamarreño <galder@zamarreno.com>
2017-06-23 17:50:45 +0200

    [HB] CH20 - foldMap vs foldr

--- haskellbook/foldable-ch20.hb/RevengeOfMonoids.hs
+++ haskellbook/foldable-ch20.hb/RevengeOfMonoids.hs
@@ -16,3 +16,14 @@ main =
         print $ foldMap Any [(3 == 4), (9 > 5)]
         print $ foldMap First [Just 1, Nothing, Just 5]
         print $ foldMap Last [Just 1, Nothing, Just 5]
+        print $ foldMap (*5) [1, 2, 3 :: Product Integer]
+        print $ foldMap (*5) [1, 2, 3 :: Sum Integer]
+        print $ foldr (*) 5 [1, 2, 3]
+        -- The way foldr works, declaring a Monoid instance
+        -- different from what is implied in the folding function
+        -- doesn't change the result
+        print $ foldr (*) 3 [1, 2, 3 :: Sum Integer]
+        print $ foldr (*) 3 [1, 2, 3 :: Product Integer]
+        -- With one fold element, the same applies to foldMap
+        print (foldMap (*5) (Just 100) :: Product Integer)
+        print (foldMap (*5) (Just 5) :: Sum Integer)
