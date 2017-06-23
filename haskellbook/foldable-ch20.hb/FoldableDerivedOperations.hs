773fe88c3ab480f1b99f805f6184798c6dfea44d
Galder Zamarreño <galder@zamarreno.com>
2017-06-23 18:28:06 +0200

    [HB] CH20 - Foldable length

--- haskellbook/foldable-ch20.hb/FoldableDerivedOperations.hs
+++ haskellbook/foldable-ch20.hb/FoldableDerivedOperations.hs
@@ -14,3 +14,9 @@ main =
         print $ null Nothing
         print $ null (1, 2)
         print $ fmap null [Just 1, Just 2, Nothing]
+        print $ length (1, 2)
+        print $ length [(1, 2), (3, 4), (5, 6)]
+        print $ fmap length [(1, 2), (3, 4), (5, 6)]
+        print $ fmap length Just [1, 2, 3]
+        print $ fmap length [Just 1, Just 2, Just 3]
+        print $ fmap length [Just 1, Just 2, Nothing]
