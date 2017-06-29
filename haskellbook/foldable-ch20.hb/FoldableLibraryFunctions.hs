ff22023f55169d002b18f79bbd494c6c25b4fe55
Galder Zamarreño <galder@zamarreno.com>
2017-06-29 11:28:40 +0200

    [HB] CH20 - minimum in terms of foldr

--- haskellbook/foldable-ch20.hb/FoldableLibraryFunctions.hs
+++ haskellbook/foldable-ch20.hb/FoldableLibraryFunctions.hs
@@ -1,6 +1,6 @@
 import Data.Foldable (foldMap, foldr)
 import Data.Monoid
-import Prelude hiding (elem, product, sum)
+import Prelude hiding (elem, minimum, product, sum)
 
 
 -- Nicer with foldMap than foldr
@@ -23,6 +23,14 @@ elem x =
     -- foldr (\a z -> (a == x) || z) False
 
 
+minimum :: (Foldable t, Ord a) => t a -> Maybe a
+minimum xs =
+    foldr f Nothing xs
+    -- foldr (\a z -> if ((Just a) < z) then (Just a) else z) Nothing xs
+    where f a Nothing = Just a
+          f a (Just x) = if (a < x) then Just a else Just x
+
+
 main :: IO ()
 main =
     do  print $ 5 == sum (7, 5)
@@ -37,3 +45,9 @@ main =
         print $ False == elem True (Right False)
         print $ True == elem True (Right True)
         print $ [False, False, True] == fmap (elem 3) [Right 1, Right 2, Right 3]
+        print $ Just 1 < Just 2
+        print $ Just 'e' == minimum "julie"
+        print $ Just (Just 'e') == fmap minimum (Just "julie")
+        print $ Just 'j' == minimum (Just 'j')
+        print $ [Just 'j', Just 'u', Just 'l'] ==
+            fmap minimum [Just 'j', Just 'u', Just 'l']
