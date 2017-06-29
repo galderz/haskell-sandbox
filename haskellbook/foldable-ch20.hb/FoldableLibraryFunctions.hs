aff94baee5255feaeffd2f96ef5e9651b47074e9
Galder Zamarreño <galder@zamarreno.com>
2017-06-29 09:53:19 +0200

    [HB] CH20 - product in terms of foldMap/foldr

--- haskellbook/foldable-ch20.hb/FoldableLibraryFunctions.hs
+++ haskellbook/foldable-ch20.hb/FoldableLibraryFunctions.hs
@@ -1,6 +1,6 @@
 import Data.Foldable (foldMap, foldr)
 import Data.Monoid
-import Prelude hiding (sum)
+import Prelude hiding (sum, product)
 
 
 -- Nicer with foldMap than foldr
@@ -10,8 +10,18 @@ sum =
     -- foldr (+) 0
 
 
+-- Nicer with foldMap than foldr
+product :: (Foldable t, Num a) => t a -> a
+product =
+    getProduct . foldMap Product
+    -- foldr (*) 1
+
+
 main :: IO ()
 main =
     do  print $ 5 == sum (7, 5)
         print $ [5, 4] == fmap sum [(7, 5), (3, 4)]
         print $ Just 15 == fmap sum (Just [1, 2, 3, 4, 5])
+        print $ 1 == product Nothing
+        print $ Just 1 == fmap product (Just [])
+        print $ Right 6 == fmap product (Right [1, 2, 3] :: Either String [Int])
