bffb77051c7af7a5c89f88f12275f2f21af6363d
Galder Zamarreño <galder@zamarreno.com>
2017-07-05 14:45:21 +0200

    [HB] CH20 - foldl for Two

--- haskellbook/foldable-ch20.hb/FoldableExercises.hs
+++ haskellbook/foldable-ch20.hb/FoldableExercises.hs
@@ -5,6 +5,10 @@ data Constant a b =
     Constant a
 
 
+data Two a b =
+    Two a b
+
+
 instance Foldable (Constant a) where
     foldl f z (Constant x) =
         -- Can't be `f z x` because x is part of the container
@@ -21,9 +25,22 @@ instance Foldable (Constant a) where
         mempty
 
 
+instance Foldable (Two a) where
+    foldl f z (Two a x) =
+        f z x
+
+    foldr =
+        undefined
+
+    foldMap =
+        undefined
+
+
 main :: IO ()
 main =
     do  print $ 5 == foldl (*) 5 (Constant 5)
         print $ 5 == foldr (*) 5 (Constant 5)
         print $ Product 1 == (foldMap (*5) (Constant 100) :: Product Integer)
         print $ Sum 0 == (foldMap (*5) (Constant 100) :: Sum Integer)
+        print $ 25 == (foldl (*) 5 $ Two 1 5)
+        print $ 5 == (foldl (*) 5 $ Two 5 1)
