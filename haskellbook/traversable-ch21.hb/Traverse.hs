811a5483ca25d4f53bf0ad88ea0e000a70de9c29
Galder Zamarreño <galder@zamarreno.com>
2017-07-07 18:42:29 +0200

    [HB] CH21 - stringToMorse with traverse

--- haskellbook/traversable-ch21.hb/Traverse.hs
+++ haskellbook/traversable-ch21.hb/Traverse.hs
@@ -1,9 +1,78 @@
+import Data.Map
+
+
 f =
     undefined :: a -> Maybe b
 
 
 xs =
     undefined :: [a]
+
+
+type Morse = String
+
+
+letterToMorse :: (Map Char Morse)
+letterToMorse =
+    Data.Map.fromList
+        [ ('a', ".-")
+        , ('b', "-...")
+        , ('c', "-.-.")
+        , ('d', "-..")
+        , ('e', ".")
+        , ('f', "..-.")
+        , ('g', "--.")
+        , ('h', "....")
+        , ('i', "..")
+        , ('j', ".---")
+        , ('k', "-.-")
+        , ('l', ".-..")
+        , ('m', "--")
+        , ('n', "-.")
+        , ('o', "---")
+        , ('p', ".--.")
+        , ('q', "--.-")
+        , ('r', ".-.")
+        , ('s', "...")
+        , ('t', "-")
+        , ('u', "..-")
+        , ('v', "...-")
+        , ('w', ".--")
+        , ('x', "-..-")
+        , ('y', "-.--")
+        , ('z', "--..")
+        , ('1', ".----")
+        , ('2', "..---")
+        , ('3', "...--")
+        , ('4', "....-")
+        , ('5', ".....")
+        , ('6', "-....")
+        , ('7', "--...")
+        , ('8', "---..")
+        , ('9', "----.")
+        , ('0', "-----")
+        ]
+
+
+charToMorse :: Char -> Maybe Morse
+charToMorse c =
+    Data.Map.lookup c letterToMorse
+
+
+stringToMorse :: String -> Maybe [Morse]
+stringToMorse s =
+    -- sequence $ fmap charToMorse s
+    traverse charToMorse s
+
+
+morseToLetter :: Data.Map.Map Morse Char
+morseToLetter =
+    Data.Map.foldWithKey (flip Data.Map.insert) Data.Map.empty letterToMorse
+
+
+morseToChar :: Morse -> Maybe Char
+morseToChar m =
+    Data.Map.lookup m morseToLetter
 
 
 main :: IO ()
@@ -27,3 +96,5 @@ main =
         --
         -- :t traverse f xs
         -- traverse f xs :: Maybe [b]
+        print $ morseToChar "gobbledegook"
+        print $ morseToChar "-.-."
