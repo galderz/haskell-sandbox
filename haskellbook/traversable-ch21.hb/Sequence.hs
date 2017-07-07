4a3737cf135b11f109d9bc4929cf29f26595adc3
Galder Zamarreño <galder@zamarreno.com>
2017-07-07 17:52:15 +0200

    [HB] CH21 - sequenceA flips structures around

---
+++ haskellbook/traversable-ch21.hb/Sequence.hs
@@ -0,0 +1,12 @@
+main :: IO ()
+main =
+    do  -- Compare these:
+        print $ sum [1, 2, 3]
+        print $ fmap sum [Just 1, Just 2, Just 3]
+        print $ (fmap . fmap) sum Just [1, 2, 3]
+        print $ fmap product [Just 1, Just 2, Nothing]
+        -- To these:
+        print $ fmap Just [1, 2, 3]
+        print $ sequenceA $ fmap Just [1, 2, 3]
+        print $ sequenceA [Just 1, Just 2, Just 3]
+        print $ sequenceA [Just 1, Just 2, Nothing]
