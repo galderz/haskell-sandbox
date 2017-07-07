46b8e3fc784addc7cca38e1904d3a57187793276
Galder Zamarreño <galder@zamarreno.com>
2017-07-07 18:13:19 +0200

    [HB] CH21 - catMaybes vs sequenceA

--- haskellbook/traversable-ch21.hb/Sequence.hs
+++ haskellbook/traversable-ch21.hb/Sequence.hs
@@ -1,3 +1,6 @@
+import Data.Maybe
+
+
 main :: IO ()
 main =
     do  -- Compare these:
@@ -12,3 +15,11 @@ main =
         print $ sequenceA [Just 1, Just 2, Nothing]
         print $ fmap sum $ sequenceA [Just 1, Just 2, Just 3]
         print $ fmap product (sequenceA [Just 3, Just 4, Nothing])
+        -- catMaybes offers a different way to handle a list of Maybes
+        print $ catMaybes [Just 1, Just 2, Just 3]
+        print $ catMaybes [Just 1, Just 2, Nothing]
+        -- Using catMaybes allows you to sum (or otherwise process)
+        -- the list of Maybe values even if there's potentially
+        -- a Nothing value lurking within
+        print $ sum $ catMaybes [Just 1, Just 2, Nothing]
+        print $ fmap sum $ sequenceA [Just 1, Just 2, Nothing]
