9f28d33ea5041955b12aca7292c6e437e79b7a00
Galder Zamarreño <galder@zamarreno.com>
2017-07-07 18:34:24 +0200

    [HB] CH21 - What's traversable for?

--- haskellbook/traversable-ch21.hb/Traverse.hs
+++ haskellbook/traversable-ch21.hb/Traverse.hs
@@ -1,6 +1,29 @@
+f =
+    undefined :: a -> Maybe b
+
+
+xs =
+    undefined :: [a]
+
+
 main :: IO ()
 main =
     do  print $ fmap Just [1, 2, 3]
         print $ sequenceA $ fmap Just [1, 2, 3]
         print $ sequenceA . fmap Just $ [1, 2, 3]
         print $ traverse Just [1, 2, 3]
+        -- :t map f xs
+        -- map f xs :: [Maybe b]
+        --
+        -- What if we want a value of type Maybe [b] ?
+        -- The following will work, but we can do better:
+        --
+        -- :t sequenceA $ map f xs
+        -- sequenceA $ map f xs :: Maybe [a]
+        --
+        -- It's usually better to use travese
+        -- whenever we see a sequence or sequenceA
+        -- combined with a map or fmap:
+        --
+        -- :t traverse f xs
+        -- traverse f xs :: Maybe [b]
