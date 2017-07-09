a72ffbf9dd3c60ac2ad84d7a76fff547512ebc2e
Galder Zamarreño <galder@zamarreno.com>
2017-07-09 10:57:44 +0200

    [HB] CH21 - Use traverse to get foldMap

--- haskellbook/traversable-ch21.hb/TraversableStrength.hs
+++ haskellbook/traversable-ch21.hb/TraversableStrength.hs
@@ -1,8 +1,22 @@
+import Data.Functor.Constant
 import Data.Functor.Identity
+import Data.Monoid
 
 
 edgelordMap f t =
     runIdentity $ traverse (Identity . f) t
+
+
+xs =
+    [1, 2, 3, 4, 5] :: [Sum Integer]
+
+
+
+-- Compared to:
+-- foldMap :: (Foldable t, Monoid m)  => (a -> m) -> t a -> m
+foldMap' :: (Monoid m, Traversable t) => (a -> m) -> t a -> m
+foldMap' f t =
+    getConstant $ traverse (Constant . f) t
 
 
 main :: IO ()
@@ -10,3 +24,4 @@ main =
     do  print $ traverse (Identity . (+1)) [1, 2]
         print $ runIdentity $ traverse (Identity . (+1)) [1, 2]
         print $ edgelordMap (+1) [1..5]
+        print $ traverse (Constant . (+1)) xs
