107dfb2428558981e223ffa527e282b11a764cf1
Galder Zamarreño <galder@zamarreno.com>
2017-07-09 11:16:46 +0200

    [HB] CH21 - Traversable QuickCheck

---
+++ haskellbook/traversable-ch21.hb/QuickCheckTraversable.hs
@@ -0,0 +1,14 @@
+module QuickCheckTraversable where
+
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+
+type TI = []
+
+
+main :: IO ()
+main =
+    do  let trigger = undefined :: TI (Int, Int, [Int])
+        quickBatch (traversable trigger)
