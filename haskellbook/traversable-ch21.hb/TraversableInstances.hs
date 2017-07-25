abe36aecb83fd30d596fae062f73b70618e2a677
Galder Zamarreño <galder@zamarreno.com>
2017-07-25 19:33:07 +0200

    [HB] CH21 - Constant Functor

--- haskellbook/traversable-ch21.hb/TraversableInstances.hs
+++ haskellbook/traversable-ch21.hb/TraversableInstances.hs
@@ -8,9 +8,19 @@ newtype Identity a =
     deriving (Eq, Ord, Show)
 
 
+newtype Constant a b =
+    Constant { getConstant :: a }
+    deriving (Eq, Ord, Show)
+
+
 instance Functor Identity where
     fmap f (Identity x) =
         Identity (f x)
+
+
+instance Functor (Constant a) where
+    fmap _ (Constant a) =
+        Constant a
 
 
 instance Foldable Identity where
@@ -21,9 +31,22 @@ instance Foldable Identity where
         f x
 
 
+instance Foldable (Constant a) where
+    foldr =
+        undefined
+
+    foldMap =
+        undefined
+
+
 instance Traversable Identity where
     traverse f (Identity x) =
         fmap Identity (f x)
+
+
+instance Traversable (Constant a) where
+    traverse =
+        undefined
 
 
 instance (Arbitrary a) => Arbitrary (Identity a) where
@@ -32,7 +55,18 @@ instance (Arbitrary a) => Arbitrary (Identity a) where
             return $ Identity x
 
 
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Constant a b) where
+    arbitrary =
+        do  x <- arbitrary
+            return $ Constant x
+
+
 instance (Eq a) => EqProp (Identity a) where
+    (=-=) =
+        eq
+
+
+instance (Eq a, Eq b) => EqProp (Constant a b) where
     (=-=) =
         eq
 
@@ -40,8 +74,16 @@ instance (Eq a) => EqProp (Identity a) where
 type TI = Identity
 
 
+type TC = Constant
+
+
 main :: IO ()
 main =
-    do  let trigger = undefined :: TI (Int, Int, [Int])
-        -- quickBatch (functor trigger)
-        quickBatch (traversable trigger)
+    do  let
+            ti =
+                undefined :: TI (Int, Int, [Int])
+            tc =
+                undefined :: TC (Int, Int, [Int]) (Int, Int, [Int])
+        -- quickBatch (functor ti)
+        -- quickBatch (traversable ti)
+        quickBatch (functor tc)
