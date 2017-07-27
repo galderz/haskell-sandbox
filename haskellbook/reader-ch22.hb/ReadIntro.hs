f390192472747369eabd336b06799dc3ec321957
Galder Zamarreño <galder@zamarreno.com>
2017-07-27 16:59:13 +0200

    [HB] CH22 - Fmap function composition spelled out

--- haskellbook/reader-ch22.hb/ReadIntro.hs
+++ haskellbook/reader-ch22.hb/ReadIntro.hs
@@ -18,7 +18,13 @@ bloop =
     fmap boop doop
 
 
+bloop' :: Integer -> Integer
+bloop' x =
+    (*2) ((+10) x)
+
+
 main :: IO ()
 main =
     do  print $ bip 5
         print $ bloop 5
+        print $ bloop' 5
