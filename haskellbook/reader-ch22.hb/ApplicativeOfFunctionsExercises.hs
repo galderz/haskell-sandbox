16f7affc5c00ac47bd792fc9ba8e904f03e8c388
Galder Zamarreño <galder@zamarreno.com>
2017-08-03 15:19:42 +0200

    [HB] CH22 - asks impl

--- haskellbook/reader-ch22.hb/ApplicativeOfFunctionsExercises.hs
+++ haskellbook/reader-ch22.hb/ApplicativeOfFunctionsExercises.hs
@@ -3,3 +3,12 @@ myLiftA2 :: Applicative f =>
     -> f a -> f b -> f c
 myLiftA2 f fa fb =
     f <$> fa <*> fb
+
+
+newtype Reader r a =
+    Reader { runReader :: r -> a }
+
+
+asks :: (r -> a) -> Reader r a
+asks f =
+    Reader f
