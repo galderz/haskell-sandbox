3541855e8c94bdc56c15cdbddf00d454c50a3624
Galder Zamarreño <galder@zamarreno.com>
2017-08-08 13:48:33 +0200

    [HB] CH22 - xs lookup

--- haskellbook/reader-ch22.hb/ReaderPractice.hs
+++ haskellbook/reader-ch22.hb/ReaderPractice.hs
@@ -16,22 +16,23 @@ z =
     [7, 8, 9]
 
 
-lookup :: Eq a => a -> [(a,b)] -> Maybe b
-lookup =
-    undefined
+-- lookup :: Eq a => a -> [(a, b)] -> Maybe b
+-- lookup x l =
+--     fmap snd found
+--     where
+--         found = find (\(a, b) -> if (a == x) then True else False) l
 
 
 -- zip x and y using 3 as the lookup key
 xs :: Maybe Integer
 xs =
-    undefined
+    lookup 3 $ zip x y
 
 
 -- zip y and z using 6 as the lookup key
 ys :: Maybe Integer
 ys =
     undefined
-
 
 
 -- it's also nice to have one tha
@@ -46,3 +47,8 @@ zs =
 z' :: Integer -> Maybe Integer
 z' n =
     undefined
+
+
+main :: IO ()
+main =
+    do  print $ xs
