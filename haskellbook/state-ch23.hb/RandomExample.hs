b30eda1f9b7584082365b4abe28caba750baeb36
Galder Zamarreño <galder@zamarreno.com>
2017-08-18 15:40:59 +0200

    [HB] CH23 - intToDie method

--- haskellbook/state-ch23.hb/RandomExample.hs
+++ haskellbook/state-ch23.hb/RandomExample.hs
@@ -13,3 +13,16 @@ data Die =
     | DieFive
     | DieSix
     deriving (Eq, Show)
+
+
+intToDie :: Int -> Die
+intToDie n =
+    case n of
+        1 -> DieOne
+        2 -> DieTwo
+        3 -> DieThree
+        4 -> DieFour
+        5 -> DieFive
+        6 -> DieSix
+        -- Use this tactic _extremely_ sparingly.
+        x -> error $ "intToDie got non 1-6 integer: " ++ show x
