9eb9a206a0e7b3761f4cce742646ccb30c4a34a8
Galder Zamarreño <galder@zamarreno.com>
2017-08-18 15:42:56 +0200

    [HB] CH23 - rollDieThreeTimes

--- haskellbook/state-ch23.hb/RandomExample.hs
+++ haskellbook/state-ch23.hb/RandomExample.hs
@@ -26,3 +26,17 @@ intToDie n =
         6 -> DieSix
         -- Use this tactic _extremely_ sparingly.
         x -> error $ "intToDie got non 1-6 integer: " ++ show x
+
+
+-- Works, but not optimal
+rollDieThreeTimes :: (Die, Die, Die)
+rollDieThreeTimes =
+    do
+        -- this will produce the same results every
+        -- time because it is free of effects.
+        -- This is fine for this demonstration.
+        let s = mkStdGen 0
+            (d1, s1) = randomR (1, 6) s
+            (d2, s2) = randomR (1, 6) s1
+            (d3, _) = randomR (1, 6) s2
+        (intToDie d1, intToDie d2, intToDie d3)
