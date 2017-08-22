2464b59fe83b4eaad1688fa80c9bc501f2329f6d
Galder Zamarreño <galder@zamarreno.com>
2017-08-22 16:23:41 +0200

    [HB] CH23 - rollsToGetN with limit as parameter

--- haskellbook/state-ch23.hb/RandomExample2.hs
+++ haskellbook/state-ch23.hb/RandomExample2.hs
@@ -88,3 +88,19 @@ rollsToGetTwenty g =
                   let (die, nextGen) =
                           randomR (1, 6) gen
                   in go (sum + die) (count + 1) nextGen
+
+
+-- Example:
+-- (rollsToGetN 20 . mkStdGen) <$> randomIO
+rollsToGetN :: Int -> StdGen -> Int
+rollsToGetN n g =
+    go 0 0 g
+    where
+        go :: Int -> Int -> StdGen -> Int
+        go sum count gen
+            | sum >= n =
+                  count
+            | otherwise =
+                  let (die, nextGen) =
+                          randomR (1, 6) gen
+                  in go (sum + die) (count + 1) nextGen
