229b0e95487f1a19801e8626dd7b5a9c8d70ca15
Galder Zamarreño <galder@zamarreno.com>
2017-08-22 17:14:13 +0200

    [HB] CH23 - rollsCountLogged impl

--- haskellbook/state-ch23.hb/RandomExample2.hs
+++ haskellbook/state-ch23.hb/RandomExample2.hs
@@ -104,3 +104,20 @@ rollsToGetN n g =
                   let (die, nextGen) =
                           randomR (1, 6) gen
                   in go (sum + die) (count + 1) nextGen
+
+
+-- Example:
+-- (rollsCountLogged 20 . mkStdGen) <$> randomIO
+rollsCountLogged :: Int -> StdGen -> (Int, [Die])
+rollsCountLogged n g =
+    go 0 0 g []
+    where
+        go :: Int -> Int -> StdGen -> [Die] -> (Int, [Die])
+        go sum count gen ds
+            | sum >= n =
+                  (count, ds)
+            | otherwise =
+                  let (die, nextGen) =
+                          randomR (1, 6) gen
+                  in
+                      go (sum + die) (count + 1) nextGen (intToDie die : ds)
