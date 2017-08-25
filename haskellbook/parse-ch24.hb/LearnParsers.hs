6dca13f9bdf82dc35bf108c2350800b3a48e2685
Galder Zamarreño <galder@zamarreno.com>
2017-08-25 15:54:46 +0200

    [HB] CH24 - Create a test parser

--- haskellbook/parse-ch24.hb/LearnParsers.hs
+++ haskellbook/parse-ch24.hb/LearnParsers.hs
@@ -23,3 +23,39 @@ one =
 one' :: Parser b
 one' =
     one >> stop
+
+
+-- read two characters, '1', and '2'
+oneTwo :: Parser Char
+oneTwo =
+    char '1' >> char '2'
+
+
+-- read two characters,
+-- '1' and '2', then die
+oneTwo' :: Parser b
+oneTwo' =
+    oneTwo >> stop
+
+
+testParse :: Parser Char -> IO ()
+testParse p =
+    print $ parseString p mempty "123"
+
+
+pNL s =
+    putStrLn ('\n' : s)
+
+
+main :: IO ()
+main =
+    do  pNL "stop:"
+        testParse stop
+        pNL "one:"
+        testParse one
+        pNL "one':"
+        testParse one'
+        pNL "oneTwo:"
+        testParse oneTwo
+        pNL "oneTwo':"
+        testParse oneTwo'
