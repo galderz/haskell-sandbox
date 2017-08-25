4a467179f11d301e82939836f06b7d96ad1e47aa
Galder Zamarreño <galder@zamarreno.com>
2017-08-25 15:37:28 +0200

    [HB] CH24 - Combine basic parsers

--- haskellbook/parse-ch24.hb/LearnParsers.hs
+++ haskellbook/parse-ch24.hb/LearnParsers.hs
@@ -7,3 +7,19 @@ import Text.Trifecta
 stop :: Parser a
 stop =
     unexpected "stop"
+
+
+-- read a single character '1'
+one :: Parser Char
+one =
+    char '1'
+
+
+-- read a single character '1', then die
+-- equivalent to char '1' >> stop
+--
+-- (>>) :: Monad m => m a -> m b -> m b
+-- use >> instead of >>= because result is being thrown away
+one' :: Parser b
+one' =
+    one >> stop
