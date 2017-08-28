3f6ac202cfac599ba862b8c320c83e747a0b6104
Galder Zamarreño <galder@zamarreno.com>
2017-08-28 18:23:32 +0200

    [HB] CH24 - parseString with a combo of parsers

--- haskellbook/parse-ch24.hb/ParsingFreeJazz.hs
+++ haskellbook/parse-ch24.hb/ParsingFreeJazz.hs
@@ -7,3 +7,5 @@ main =
         let gimmeA = char 'a'
         print $ parseString gimmeA mempty "a"
         print $ parseString gimmeA mempty "b"
+        print $ parseString (char 'b') mempty "b"
+        print $ parseString (char 'b' >> char 'c') mempty "b"
