c46ef6437c916524cf9da6a203c27a00a49c40f4
Galder Zamarreño <galder@zamarreno.com>
2017-08-30 16:41:03 +0200

    [HB] CH24 - Parsers only consume as much as they need

--- haskellbook/parse-ch24.hb/ParsingFreeJazz.hs
+++ haskellbook/parse-ch24.hb/ParsingFreeJazz.hs
@@ -14,3 +14,8 @@ main =
         print $ parseString (string "abc") mempty "abc"
         print $ parseString (string "abc") mempty "bc"
         print $ parseString (string "abc") mempty "ab"
+        print $ parseString (char 'a') mempty "abcdef"
+        let stop = unexpected "stop pls"
+        print $ (parseString (char 'a' >> stop) mempty "abcdef" :: Result String)
+        print $ parseString (string "abc") mempty "abcdef"
+        print $ (parseString (string "abc" >> stop) mempty "abcdef" :: Result String)
