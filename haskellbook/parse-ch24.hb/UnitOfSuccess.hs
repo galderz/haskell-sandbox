65554c6466d27826c08cdb28d3c780299abab0ad
Galder Zamarreño <galder@zamarreno.com>
2017-08-31 16:32:27 +0200

    [HB] CH24 - Return integer instead of returning ()

--- haskellbook/parse-ch24.hb/UnitOfSuccess.hs
+++ haskellbook/parse-ch24.hb/UnitOfSuccess.hs
@@ -6,3 +6,4 @@ main =
     do  print $ parseString integer mempty "123abc"
         print $ parseString (integer >> eof) mempty "123abc"
         print $ parseString (integer >> eof) mempty "123"
+        print $ parseString (integer >>= (\x -> eof >> pure x)) mempty "123"
