8c5ba345845258a22353666e93ff7424411ae0de
Galder Zamarreño <galder@zamarreno.com>
2017-09-04 18:18:57 +0200

    [HB] CH24 - Parse .ini file header

--- haskellbook/parse-ch24.hb/IniParsing.hs
+++ haskellbook/parse-ch24.hb/IniParsing.hs
@@ -14,3 +14,27 @@ import Test.Hspec
 import Text.RawString.QQ
 -- parsers 0.12.3, trifecta 1.5.2
 import Text.Trifecta
+
+
+headerEx :: ByteString
+headerEx =
+    "blah"
+
+
+-- "[blah]" -> Section "blah"
+newtype Header =
+    Header String
+    deriving (Eq, Ord, Show)
+
+
+parseBracketPair :: Parser a -> Parser a
+parseBracketPair p =
+    char '[' *> p <* char ']'
+    -- these operators mean the brackets
+    -- will be parsed and then discarded
+    -- but the p will remain as our result
+
+
+parseHeader :: Parser Header
+parseHeader =
+    parseBracketPair (Header <$> some letter)
