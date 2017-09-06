8d37614df6c141a60a345c71ca46244cac4c743e
Galder Zamarreño <galder@zamarreno.com>
2017-09-06 17:07:01 +0200

    [HB] CH24 - token digit does not do what you expect

--- haskellbook/parse-ch24.hb/CharTokenParsing.hs
+++ haskellbook/parse-ch24.hb/CharTokenParsing.hs
@@ -10,3 +10,7 @@ main =
         print $ parseString (some integer) mempty "123 456"
         print $ parseString (some integer) mempty "123\n\n 456"
         print $ parseString (some integer) mempty "123 \n \n 456"
+        let s =
+                "123 \n \n 456"
+        print $ parseString (token (some digit)) mempty s
+        print $ parseString (token (some (token digit))) mempty s
