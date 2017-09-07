131e20950358f72338d84fed2f6c35bd403587f3
Galder Zamarreño <galder@zamarreno.com>
2017-09-07 14:44:06 +0200

    [HB] CH24 - Parser backtracking example imports

---
+++ haskellbook/parse-ch24.hb/BackTracking.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module BT where
+
+
+import Control.Applicative
+import qualified Data.Attoparsec.ByteString as A
+import Data.Attoparsec.ByteString (parseOnly)
+import Data.ByteString (ByteString)
+import Text.Trifecta hiding (parseTest)
+import Text.Parsec (Parsec, parseTest)
