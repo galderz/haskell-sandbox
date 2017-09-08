b0c9996b69e8b0783f28434375e4fcd2dc40cc6b
Galder Zamarreño <galder@zamarreno.com>
2017-09-08 17:12:04 +0200

    [HB] CH24 - Use lazy ByteString

--- haskellbook/parse-ch24.hb/AesonMarshalling.hs
+++ haskellbook/parse-ch24.hb/AesonMarshalling.hs
@@ -6,3 +6,10 @@ module AesonMarshalling where
 import Data.Aeson
 import Data.ByteString.Lazy (ByteString)
 import Text.RawString.QQ
+
+
+sectionJson :: ByteString
+sectionJson = [r|
+{ "section": {"host": "wikipedia.org"},
+  "whatisit": {"red": "intoothandclaw"} }
+|]
