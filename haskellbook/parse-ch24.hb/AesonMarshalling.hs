aba7aef31a5a3cf111526e87a15ed4e59e9e1902
Galder Zamarreño <galder@zamarreno.com>
2017-09-08 17:10:04 +0200

    [HB] CH24 - Initial aeson marsahlling example

---
+++ haskellbook/parse-ch24.hb/AesonMarshalling.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+
+module AesonMarshalling where
+
+import Data.Aeson
+import Data.ByteString.Lazy (ByteString)
+import Text.RawString.QQ
