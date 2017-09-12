bb0fc83ae06a4ee7324a925d2dea8b44992dba1e
Galder Zamarreño <galder@zamarreno.com>
2017-09-12 07:37:41 +0200

    [HB] CH24 - Initial number or string JSON parser

--- haskellbook/parse-ch24.hb/NumberOfString.hs
+++ haskellbook/parse-ch24.hb/NumberOfString.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Control.Applicative
 import Data.Aeson
 import Data.ByteString.Lazy (ByteString)
@@ -5,3 +7,40 @@ import qualified Data.Text as T
 import Data.Text (Text)
 import Text.RawString.QQ
 import Data.Scientific (floatingOrInteger)
+
+
+data NumberOrString =
+    Numba Integer
+    | Stringy Text
+    deriving (Eq, Show)
+
+
+instance FromJSON NumberOrString where
+    parseJSON (Number i) =
+        case floatingOrInteger i of
+            (Left _) ->
+                fail "Must be integral number"
+            (Right integer) ->
+                return $ Numba integer
+
+    parseJSON (String s) =
+        return $ Stringy s
+
+    parseJSON _ =
+        fail "NumberOrString must\
+             \ be number or string"
+
+
+-- so it knows what we want to parse
+dec :: ByteString -> Maybe NumberOrString
+dec =
+    decode
+
+
+eitherDec :: ByteString -> Either String NumberOrString
+eitherDec =
+    eitherDecode
+
+
+main =
+    do  print $ dec "blah"
