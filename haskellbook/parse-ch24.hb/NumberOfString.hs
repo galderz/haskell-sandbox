c66447b603f839cd7da5e8477a02ba0cf1e878e0
Galder Zamarreño <galder@zamarreno.com>
2017-09-12 07:29:51 +0200

    [HB] CH24 - Imports for number or string JSON example

---
+++ haskellbook/parse-ch24.hb/NumberOfString.hs
@@ -0,0 +1,7 @@
+import Control.Applicative
+import Data.Aeson
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.Text as T
+import Data.Text (Text)
+import Text.RawString.QQ
+import Data.Scientific (floatingOrInteger)
