d99edc4d0fdbf47170546839ff73af35eb8e045c
Galder Zamarreño <galder@zamarreno.com>
2017-09-21 17:53:46 +0200

    [HB] CH24 - Parse numbers that have no separator

--- haskellbook/parse-ch24.hb/PhoneNumberParsing.hs
+++ haskellbook/parse-ch24.hb/PhoneNumberParsing.hs
@@ -1,5 +1,6 @@
+import Control.Applicative
+import Data.Char (digitToInt)
 import Text.Trifecta
-
 
 -- aka area code
 type NumberingPlanArea =
@@ -19,18 +20,39 @@ data PhoneNumber =
     deriving (Eq, Show)
 
 
+parseSeparator :: Parser ()
+parseSeparator =
+    try $ skipMany (oneOf "-")
+    <|> return ()
+
+
+parseThreeDigits :: Parser Int
+parseThreeDigits =
+    do  n1 <- digit
+        n2 <- digit
+        n3 <- digit
+        return $ stringToInt (n1 : n2 : n3 : [])
+
+
+stringToInt :: String -> Int
+stringToInt s =
+    foldl (\z x -> (z * 10) + digitToInt x) 0 s
+
+
 parsePhone :: Parser PhoneNumber
 parsePhone =
-    do  area <- decimal
-        char '-'
-        exchange <- decimal
-        char '-'
+    do  area <- parseThreeDigits
+        parseSeparator
+        exchange <- parseThreeDigits
+        parseSeparator
         number <- decimal
-        return $ PhoneNumber
-            (fromInteger area) (fromInteger exchange) (fromInteger number)
+        return $ PhoneNumber area exchange (fromInteger number)
 
 
 main :: IO ()
 main =
     do  print $ parseString parsePhone mempty "123-456-7890"
         -- Success (PhoneNumber 123 456 7890)
+
+        print $ parseString parsePhone mempty "1234567890"
+        -- Success (PhoneNumber 123 456 7890
