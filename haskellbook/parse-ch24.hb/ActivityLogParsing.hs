0ebc44dd9f54bacde08e6662b57687abf59f608e
Galder Zamarreño <galder@zamarreno.com>
2017-09-27 17:46:46 +0200

    [HB] CH24 - Test full log, and test activity followed by comment

--- haskellbook/parse-ch24.hb/ActivityLogParsing.hs
+++ haskellbook/parse-ch24.hb/ActivityLogParsing.hs
@@ -142,48 +142,48 @@ main :: IO ()
 main =
     hspec $
     do
-        -- describe "Log parsing:" $ do
-        --     it "can parse a full log" $ do
-        --         let p =
-        --                 parseLog
-        --             i =
-        --                 sampleLog
-        --             m =
-        --                 parseByteString p mempty i
-        --             r' =
-        --                 maybeSuccess m
-        --         print m
-        --         r' `shouldBe` Just (
-        --             Log [
-        --                     ("2025-02-05",
-        --                      [
-        --                          (800, "Breakfast")
-        --                          , (900, "Sanitizing moisture collector")
-        --                          , (1100, "Exercising in high-grav gym")
-        --                          , (1200, "Lunch")
-        --                          , (1300, "Programming")
-        --                          , (1700, "Commuting home in rover")
-        --                          , (1730, "R&R")
-        --                          , (1900, "Dinner")
-        --                          , (2100, "Shower")
-        --                          , (2115, "Read")
-        --                          , (2200, "Sleep")
-        --                      ])
-        --                     , ("2025-02-07",
-        --                        [
-        --                            (800, "Breakfast")
-        --                            , (900, "Bumped head, passed out")
-        --                            , (1336, "Wake up, headache")
-        --                            , (1337, "Go to medbay")
-        --                            , (1340, "Patch self up")
-        --                            , (1345, "Commute home for rest")
-        --                            , (1415, "Read")
-        --                            , (2100, "Dinner")
-        --                            , (2115, "Read")
-        --                            , (2200, "Sleep")
-        --                        ])
-        --                 ]
-        --             )
+        describe "Log parsing:" $ do
+            it "can parse a full log" $ do
+                let p =
+                        parseLog
+                    i =
+                        sampleLog
+                    m =
+                        parseByteString p mempty i
+                    r' =
+                        maybeSuccess m
+                print m
+                r' `shouldBe` Just (
+                    Log [
+                            ("2025-02-05",
+                             [
+                                 (800, "Breakfast")
+                                 , (900, "Sanitizing moisture collector")
+                                 , (1100, "Exercising in high-grav gym")
+                                 , (1200, "Lunch")
+                                 , (1300, "Programming")
+                                 , (1700, "Commuting home in rover")
+                                 , (1730, "R&R")
+                                 , (1900, "Dinner")
+                                 , (2100, "Shower")
+                                 , (2115, "Read")
+                                 , (2200, "Sleep")
+                             ])
+                            , ("2025-02-07",
+                               [
+                                   (800, "Breakfast")
+                                   , (900, "Bumped head, passed out")
+                                   , (1336, "Wake up, headache")
+                                   , (1337, "Go to medbay")
+                                   , (1340, "Patch self up")
+                                   , (1345, "Commute home for rest")
+                                   , (1415, "Read")
+                                   , (2100, "Dinner")
+                                   , (2115, "Read")
+                                   , (2200, "Sleep")
+                               ])
+                        ]
+                    )
 
         describe "Comment parsing:" $ do
             it "skips comment before date" $ do
@@ -278,6 +278,19 @@ main =
                         maybeSuccess m
                 print m
                 r' `shouldBe` Just (800, "Gym")
+
+            it "can parse a day followed by a single activity, followed by comment" $ do
+                let p =
+                        skipWhitespace >> skipComments >> parseDate
+                        >> parseActivity
+                    i =
+                        "# 2025-02-05\n08:00 Breakfast -- are you sure?"
+                    m =
+                        parseByteString p mempty i
+                    r' =
+                        maybeSuccess m
+                print m
+                r' `shouldBe` Just (800, "Breakfast")
 
 
 activityDay :: ByteString
