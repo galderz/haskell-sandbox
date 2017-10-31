0cda15adcc4105355af31296160b8d52f602e9c6
Galder Zamarreño <galder@zamarreno.com>
2017-10-31 17:52:32 +0100

    [HB] CH24 - Test to verify parsing an IPv4 address

--- haskellbook/parse-ch24.hb/IpV4Address.hs
+++ haskellbook/parse-ch24.hb/IpV4Address.hs
@@ -9,6 +9,13 @@ data IPAddress =
     deriving (Eq, Ord, Show)
 
 
+maybeSuccess :: Result a -> Maybe a
+maybeSuccess (Success a) =
+    Just a
+maybeSuccess _ =
+    Nothing
+
+
 dec1 :: Bits a => a -> a
 dec1 n =
     shift n 24
@@ -45,6 +52,18 @@ main :: IO ()
 main =
     hspec $
     do
+        describe "IPv4 parsing: " $ do
+            it "can parse 172.16.254.1" $ do
+                let p =
+                        parseIPAddress
+                    i =
+                        "172.16.254.1"
+                    m =
+                        parseString p mempty i
+                    r' =
+                        maybeSuccess m
+                r' `shouldBe` Just (IPAddress 2886794753)
+
         describe "Shifting numbers: " $ do
             it "can calculate an IP address by shifting and adding numbers" $ do
                 let dec =
