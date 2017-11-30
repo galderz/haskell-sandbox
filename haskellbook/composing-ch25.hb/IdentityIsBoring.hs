c5a6836575017fd6044eea445ea9d0cf58a50c15
Galder Zamarreño <galder@zamarreno.com>
2017-11-30 18:23:07 +0100

    [HB] CH25 - Compose type

--- haskellbook/composing-ch25.hb/IdentityIsBoring.hs
+++ haskellbook/composing-ch25.hb/IdentityIsBoring.hs
@@ -1,2 +1,16 @@
 newtype Identity a =
     Identity { runIdentity :: a }
+
+
+newtype Compose f g a =
+    Compose { getCompose :: f (g a) }
+    deriving (Eq, Show)
+
+
+main :: IO ()
+main =
+  do  print $ Compose [Just 1, Nothing]
+      let xs =
+              [Just (1::Int), Nothing]
+      -- Compose [] Maybe Int
+      print $ Compose xs
