d8051591d4434ad1956bf10df41fc9e803f54539
Galder Zamarreño <galder@zamarreno.com>
2017-11-30 18:53:39 +0100

    [HB] CH25 - Compose Applicative pure

--- haskellbook/composing-ch25.hb/Composing.hs
+++ haskellbook/composing-ch25.hb/Composing.hs
@@ -22,8 +22,8 @@ instance (Functor f, Functor g) => Functor (Compose f g) where
 
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
     pure :: a -> Compose f g a
-    pure =
-        undefined
+    pure x =
+        Compose $ (pure . pure) x
 
     (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
     (Compose f) <*> (Compose a) =
