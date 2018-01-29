c5d61b1b067da9f0d918c6d8ac7faba74df3d77f
Galder Zamarreño <galder@zamarreno.com>
2018-01-29 17:58:29 +0100

    [HB] CH25 - fmap and join is >>=

--- haskellbook/composing-ch25.hb/MonadTransformerStepByStep.hs
+++ haskellbook/composing-ch25.hb/MonadTransformerStepByStep.hs
@@ -31,9 +31,12 @@ instance (Monad m) => Monad (IdentityT m) where
     (>>=) :: IdentityT m a -> (a -> IdentityT m b) -> IdentityT m b
     (IdentityT ma) >>= f =
         let
+            -- fmap and join is >>=
+            aimb = ma >>= runIdentityT . f
+
             -- Functor law:
             -- fmap (f . g) == fmap f . fmap g
-            aimb = join (fmap (runIdentityT . f) ma)
+            -- aimb = join (fmap (runIdentityT . f) ma)
         in
             IdentityT aimb
 
