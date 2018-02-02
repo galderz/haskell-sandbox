f1d5ec210be2a657051e22d568aec98fe001ec9d
Galder Zamarreño <galder@zamarreno.com>
2018-02-02 20:57:06 +0100

    [HB] CH26 - Functor for Compose

--- haskellbook/transformers-ch26.hb/MaybeT.hs
+++ haskellbook/transformers-ch26.hb/MaybeT.hs
@@ -5,3 +5,8 @@ newtype MaybeT m a =
 newtype Compose f g a =
     Compose { getCompose :: f (g a) }
     deriving (Eq, Show)
+
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+    fmap f (Compose fga) =
+        Compose $ (fmap . fmap) f fga
