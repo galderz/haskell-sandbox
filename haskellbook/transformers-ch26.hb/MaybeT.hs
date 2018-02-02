f33e7ff0f49ec6207377e17713e658e9e95cd9aa
Galder Zamarreño <galder@zamarreno.com>
2018-02-02 21:01:55 +0100

    [HB] CH26 - Functor for MaybeT

--- haskellbook/transformers-ch26.hb/MaybeT.hs
+++ haskellbook/transformers-ch26.hb/MaybeT.hs
@@ -10,3 +10,8 @@ newtype Compose f g a =
 instance (Functor f, Functor g) => Functor (Compose f g) where
     fmap f (Compose fga) =
         Compose $ (fmap . fmap) f fga
+
+
+instance (Functor m) => Functor (MaybeT m) where
+    fmap f (MaybeT ma) =
+        MaybeT $ (fmap . fmap) f ma
