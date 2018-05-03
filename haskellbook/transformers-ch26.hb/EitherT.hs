51bea48e21be7ab1957659ad2b63c57613b67d57
Galder Zamarreño <galder@zamarreno.com>
2018-05-03 17:56:39 +0200

    [HB] CH26 - Functor for EitherT

--- haskellbook/transformers-ch26.hb/EitherT.hs
+++ haskellbook/transformers-ch26.hb/EitherT.hs
@@ -1,4 +1,9 @@
--- {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE InstanceSigs #-}
 
 newtype EitherT e m a =
      EitherT { runEitherT :: m (Either e a) }
+
+
+instance Functor m => Functor (EitherT e m) where
+    fmap f (EitherT emx) =
+        EitherT $ (fmap . fmap) f emx
