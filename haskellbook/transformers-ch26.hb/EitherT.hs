e6add5c7f10215eb9c5ec993df099ea0a3dea269
Galder Zamarreño <galder@zamarreno.com>
2018-05-05 22:35:19 +0200

    [HB] CH26 - either catamorphism

--- haskellbook/transformers-ch26.hb/EitherT.hs
+++ haskellbook/transformers-ch26.hb/EitherT.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE InstanceSigs #-}
 
+import Control.Monad (join)
+
+
 newtype EitherT e m a =
      EitherT { runEitherT :: m (Either e a) }
 
@@ -42,3 +45,15 @@ swapEither (Left x) =
     Right x
 swapEither (Right y) =
     Left y
+
+
+eitherT :: Monad m => (a -> m c) -> (b -> m c) -> EitherT a m b -> m c
+eitherT =
+    undefined
+
+
+either' :: (a -> c) -> (b -> c) -> Either a b -> c
+either' fx _ (Left x) =
+    fx x
+either' _ fy (Right y) =
+    fy y
