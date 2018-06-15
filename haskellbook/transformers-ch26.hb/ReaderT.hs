2b9382bbce17249c552b468a7111702d6af4b444
Galder Zamarreño <galder@zamarreno.com>
2018-06-15 08:04:21 +0200

    [HB] CH26 - ReaderT applicative pure

--- haskellbook/transformers-ch26.hb/ReaderT.hs
+++ haskellbook/transformers-ch26.hb/ReaderT.hs
@@ -5,3 +5,11 @@ newtype ReaderT r m a =
 instance Functor m => Functor (ReaderT r m) where
     fmap f (ReaderT rmx) =
         ReaderT $ (fmap . fmap) f rmx
+
+
+instance Applicative m => Applicative (ReaderT r m) where
+    pure x =
+        ReaderT (pure . pure x)
+
+    (<*>) =
+        undefined
