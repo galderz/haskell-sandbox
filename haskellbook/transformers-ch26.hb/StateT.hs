cff22c534582ea3e016e8d8f984968abdc028132
Galder Zamarreño <galder@zamarreno.com>
2018-08-01 18:20:10 +0100

    [HB] CH26 - Functor for StateT

---
+++ haskellbook/transformers-ch26.hb/StateT.hs
@@ -0,0 +1,14 @@
+import Data.Bifunctor
+
+
+newtype StateT s m a =
+    StateT { runStateT :: s -> m (a, s) }
+
+
+instance (Functor m) => Functor (StateT s m) where
+    fmap f (StateT sma) =
+        StateT $ \s ->
+                     let r =
+                             sma s
+                     in
+                         fmap (first f) r
