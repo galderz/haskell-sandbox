d6d7506833dff095956a46ebed6ceeb4dd7b03c3
Galder Zamarreño <galder@zamarreno.com>
2018-09-11 07:12:49 +0200

    [HB] CH26 - Applicative pure for StateT

--- haskellbook/transformers-ch26.hb/StateT.hs
+++ haskellbook/transformers-ch26.hb/StateT.hs
@@ -13,3 +13,11 @@ instance (Functor m) => Functor (StateT s m) where
                          sma s
                  in
                      fmap (first f) r
+
+
+instance Applicative m => Applicative (StateT s m) where
+    pure x =
+        StateT $ \s -> pure (x, s)
+
+    (<*>) =
+        undefined
